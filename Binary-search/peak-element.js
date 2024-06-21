14782354555a97c2d7ad7a7ab3b71c0f1fd741ce
Anagha Biju <66214505+ab1109@users.noreply.github.com>
2024-06-21 20:41:13 +0530

    Create peak-element.js

---
+++ Binary-search/peak-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let l = 0
+    let r = nums.length - 1
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+        //left neighbor greater
+        if (mid > 0 && nums[mid] < nums[mid - 1]) {
+            r = mid - 1
+        }
+        //right neighbor greater 
+        else if (mid < nums.length-1 && nums[mid] < nums[mid + 1]) {
+            l = mid + 1
+        }
+        else {
+            return mid
+        }
+    }
+};
